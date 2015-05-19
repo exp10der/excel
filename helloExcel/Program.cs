b6f50179fca619aef9f5757546eebc7495f08276
unknown <serkin.k@tester3.industry>
2015-05-19 16:15:15 +0300

    add class import

--- helloExcel/Program.cs
+++ helloExcel/Program.cs
@@ -49,17 +49,39 @@ namespace helloExcel
                                 }
                             }
                             Console.WriteLine(cellValue);
-                        } else 
+                        }
+                        else
                         {
                             Console.WriteLine(cell.InnerText);
                         }
-                    } 
+                    }
                 }
             }
-    }
+        }
         public static SharedStringItem GetSharedStringItemById(WorkbookPart workbookPart, int id)
         {
             return workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(id);
         }
     }
+}
+
+class Import
+{
+    public string ObjectBilder { get; set; }
+    public string K { get; set; }
+    public Status Status { get; set; }
+    public double Area { get; set; }
+    public decimal PriceMeter { get; set; }
+    public decimal PriceApartment { get; set; }
+    public int CountDayArmor { get; set; }
+    public DateTime DayArmor { get; set; }
+    public int Access { get; set; }
+    public int Floor { get; set; }
+    public int LevelRoom { get; set; }
+    public string Room { get; set; }
+}
+
+public enum Status : byte
+{
+    Free, Reservations
 }
